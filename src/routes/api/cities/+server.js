d0ee52ba6b84676d035679d058dac9bbe3d98a61
regisballa <ballaregis2008@gmail.com>
2026-03-12 15:47:38 +0100

    Add GET /api/cities endpoint

--- src/routes/api/cities/+server.js
+++ src/routes/api/cities/+server.js
@@ -1,1 +1,9 @@
 import pool from '$lib/server/database.js';
+
+
+export async function GET() {
+
+    const [rows] = await pool.query('SELECT * FROM cities');
+
+    return Response.json(rows, { status: 200 });
+}

5666da1a7aeaddaf106451095d76475dd8e9712e
regisballa <ballaregis2008@gmail.com>
2026-03-13 08:17:09 +0100

    Add Basic Authentication using environment variables

--- src/routes/api/cities/[id]/+server.js
+++ src/routes/api/cities/[id]/+server.js
@@ -1,1 +1,17 @@
 import pool from '$lib/server/database.js';
+import { API_USER, API_PASSWORD } from '$env/static/private';
+
+function checkAuth(request) {
+    const auth = request.headers.get('Authorization');
+
+    if (!auth?.startsWith('Basic ')) {
+        return false;
+    }
+
+    const base64 = auth.slice(6);
+    const decoded = atob(base64);
+
+    const [user, pass] = decoded.split(':');
+
+    return user === API_USER && pass === API_PASSWORD;
+}
